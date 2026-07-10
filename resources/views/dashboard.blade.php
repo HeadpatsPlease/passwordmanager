b8a12e061de3f29f0b6815368eb0054271fc4bed
HeadpatsPlease <emuy84@gmail.com>
2026-07-10 08:53:01 +0800

    xczxzx

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,13 +125,11 @@
                                                         @csrf
                                                         <div class="mt-2 flex flex-col">
                                                             <input type="text" class="text-md text-black-500 mb-3" placeholder="siteName" name="siteName" value="{{ $acc['siteName'] }}">
-                                                            <input type="text" minlength="8" maxlength="100" class="text-md  text-black-500 mb-3" placeholder="Email/Phone#" name="email" value="{{ $acc['email'] }}">
+                                                            <input type="text" minlength="5" maxlength="100" class="text-md  text-black-500 mb-3" placeholder="Email/Phone#" name="email" value="{{ $acc['email'] }}">
                                                             <input type="text" minlength="8" maxlength="100" class="text-md text-black-500 mb-3" placeholder="Password" name="password" value="{{ $acc['password'] }}">
                                                             <input type="hidden" class="text-md text-black-500 mb-3" placeholder="id" name="save_id" value="{{ $acc['id'] }}">
                                                         </div>
-                                                        </div>
-                                                    </div>
-                                                    </div>
+
                                                         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                                             <button type="submit" command="close" class="loading-btn inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">
                                                                 <svg
@@ -161,6 +159,11 @@
                                                             <button type="button" command="close" commandfor="dialog-{{ $acc['id'] }}" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
                                                         </div>
                                                     </form>
+                                                                                                            </div>
+                                                        </div>
+                                                        </div>
+
+
                                                 </el-dialog-panel>
                                                 </div>
                                             </dialog>
